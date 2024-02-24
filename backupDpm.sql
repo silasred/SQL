4ca46f6811cecaa322dc780b5e00cda379565123
Silas Santos <148287551+silasred@users.noreply.github.com>
2024-02-23 22:35:55 -0300

    arrumando o codigo

--- backupDpm.sql
+++ backupDpm.sql
@@ -1,17 +1,11 @@
 use master
-
-
+  
 declare @now varchar(20)
-
 declare @path varchar(256)
-
 declare @filename varchar(150)
 
-
 set @now= format(getdate(),'yyyy-MM-dd-HHmm')
-
 set @path='C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Backup\'
-
 set @filename= @path + 'DPM_' + @now + '.bak'
 
 
